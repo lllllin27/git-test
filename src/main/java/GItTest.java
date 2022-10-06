a12b9d2af5fd2023200f51599cdeb9d168670351
nero12 <592285138@qq.com>
2022-10-06 10:03:40 +0800

    first committed

---
+++ src/main/java/GItTest.java
@@ -0,0 +1,9 @@
+import com.sun.prism.shader.Solid_TextureYV12_AlphaTest_Loader;
+
+import java.sql.SQLOutput;
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
